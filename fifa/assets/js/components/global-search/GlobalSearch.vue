aa582024a56980bbd4c0aa543cbe3262088bede2
Dan Gamble <dangamble89@gmail.com>
2016-03-28 11:10:59 +0100

    Added base header

---
+++ fifa/assets/js/components/global-search/GlobalSearch.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="gbs-Outer">
+    <input class="gbs-Input" type="text" placeholder="Search..">
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+
+  }
+</script>
--- fifa/assets/js/components/index.js
+++ fifa/assets/js/components/index.js
@@ -1,5 +1,7 @@
 import Builder from './builder/Builder.vue'
+import GlobalSearch from './global-search/GlobalSearch.vue'
 
 export default {
-  Builder
+  Builder,
+  GlobalSearch
 }
