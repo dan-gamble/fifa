72a6ffe54c31ab11ef6d79acbcb9dac00776a7e4
Dan Gamble <dangamble89@gmail.com>
2016-04-25 21:21:57 +0100

    Added breadcrumbs and strip to league detail

--- fifa/apps/leagues/models.py
+++ fifa/apps/leagues/models.py
@@ -1,10 +1,14 @@
+from django.core import urlresolvers
 from django.db import models
+from django.template.defaultfilters import safe
 
 from fifa.apps.models import EaAsset, TimeStampedModel
 from ..nations.models import Nation
 
 
 class League(EaAsset, TimeStampedModel, models.Model):
+    cached_url = models.CharField(max_length=1000, null=True, blank=True)
+    
     name = models.CharField(max_length=100)
     name_abbr = models.CharField(max_length=100)
     slug = models.SlugField(blank=True, null=True)
@@ -17,3 +21,22 @@ class League(EaAsset, TimeStampedModel, models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self, cached=False):
+        if self.cached_url and cached:
+            return self.cached_url
+
+        url = urlresolvers.reverse('leagues:league', kwargs={'slug': self.slug})
+
+        if url != self.cached_url:
+            self.cached_url = url
+            self.save()
+
+        return url
+
+    def detail_title(self):
+        split = self.name.split(' ')
+        last_item = split.pop()
+        split.append('<span class="hlt-Red">{}</span>'.format(last_item))
+
+        return safe(' '.join(split))
